e237417deb9d8916c4b1f73d8583540ff2418f91
Essa Hafeez <hafeezessa11@gmail.com>
2024-08-12 11:42:46 +0500

    addfunction

---
+++ div.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const ComponentName = () => {
+  return (
+    <div>
+      Content
+function useeffect()=?{
+
+    {
+
+        Hello
+    }
+}
+    </div>
+  );
+};
+
+export default ComponentName;
